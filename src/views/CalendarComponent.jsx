ac6b142a2bb2e7b5e0239b12a2b9498c214da466
Jollymar Obias <jollymarobias25@gmail.com>
2020-10-21 11:04:10 +0800

    Fix some css

--- src/views/CalendarComponent.jsx
+++ src/views/CalendarComponent.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import './CalendarComponent.scss'
-import { BiChevronLeft } from "react-icons/bi";
-import { BiChevronRight } from "react-icons/bi";
+import './CalendarComponent.scss' ;
     
 
 class CalendarComponent extends Component{
@@ -14,13 +12,16 @@ class CalendarComponent extends Component{
                     <h1>Calendar</h1>
                     <div className="border-btm"></div>
                     <br/>
-                    <div className="m-w">
-                        <span id="schedule">Schedules Calendar</span>
-                            <div className="month">
-                                <button type="button"><BiChevronLeft/></button>
-                                <span>October 2020</span>
-                                <button type="button"><BiChevronRight/></button>
-                            </div>
+                    <div className="m-w"> 
+                        <div className="month">
+                            <button type="button">
+                                <span class="material-icons">keyboard_arrow_left</span>
+                            </button>
+                            <span>October 2020</span>
+                            <button type="button">
+                                <span class="material-icons">keyboard_arrow_right</span>
+                            </button>
+                        </div>
                     </div>
                 </div>
                 <div className="week">
@@ -33,48 +34,56 @@ class CalendarComponent extends Component{
                     <span>SATURDAY</span>
                 </div>
                 <div className="date">
-                        <div className="number">1</div>
+                        <div className="number"> 1 </div>
                         <div className="number">2
                         
                             <ul>
-                                <div className="li-part-one">
+                                <div className="li-sched-blue">
                                     <li className="training">Training 101</li>
                                     <li className="time">8:00PM - 10:00PM</li>
                                 </div>
 
-                                <div className="li-part-two">
+                                <div className="li-sched-green">
                                     <li className="training">Training 101</li>
                                     <li className="time">8:00PM - 10:00PM</li>
                                 </div>
                                 
                             </ul>
                         </div>
-                        <div className="number">3</div>
-                        <div className="number">4
-
+                        <div className="number">3
                             <ul>
-                                <div className="li-part-three">
+                                <div className="li-sched-purple">
                                     <li className="training">Training 101</li>
                                     <li className="time">8:00PM - 10:00PM</li>
                                 </div>
-                                
+
+                                <div className="li-sched-orange">
+                                    <li className="training">Training 101</li>
+                                    <li className="time">8:00PM - 10:00PM</li>
+                                </div> 
                             </ul>
                         </div>
+                        <div className="number">4</div>
                         <div className="number">5</div>
                         <div className="number">6</div>
                         <div className="number">7</div>
                         <div className="number">8</div>
                         <div className="number">9</div>
                         <div className="number">10</div>
-                        <div className="number">11
+                        <div className="number">11</div>
+                        <div className="number">12
                             <ul>
-                                <div className="li-part-four">
+                                <div className="li-sched-green">
                                     <li className="training">Training 101</li>
                                     <li className="time">8:00PM - 10:00PM</li>
                                 </div>
+
+                                <div className="li-sched-blue">
+                                    <li className="training">Training 101</li>
+                                    <li className="time">8:00PM - 10:00PM</li>
+                                </div> 
                             </ul>
                         </div>
-                        <div className="number">12</div>
                         <div className="number">13</div>
                         <div className="number">14</div>
                         <div className="number">15</div>
@@ -83,7 +92,19 @@ class CalendarComponent extends Component{
                         <div className="number">18</div>
                         <div className="number">19</div>
                         <div className="number">20</div>
-                        <div className="number">21</div>
+                        <div className="number">21
+                            <ul>
+                                <div className="li-sched-green">
+                                    <li className="training">Training 101</li>
+                                    <li className="time">8:00PM - 10:00PM</li>
+                                </div>
+
+                                <div className="li-sched-blue">
+                                    <li className="training">Training 101</li>
+                                    <li className="time">8:00PM - 10:00PM</li>
+                                </div> 
+                            </ul>
+                        </div>
                         <div className="number">22</div>
                         <div className="number">23</div>
                         <div className="number">24</div>
